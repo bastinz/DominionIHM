32a6ff9d075c21bf55ff6a6c5f7b6369a78dbcb2
bastinz <sophie.nabitz@univ-avignon.fr>
2026-05-07 08:54:54 +0200

    Un test d'un cutpurse sur tous les joueurs

---
+++ src/test/java/fr/umontpellier/iut/dominionfx/basicgame/SeveralPlayersTest.java
@@ -0,0 +1,37 @@
+package fr.umontpellier.iut.dominionfx.basicgame;
+
+import fr.umontpellier.iut.dominionfx.BaseTestClass;
+import fr.umontpellier.iut.dominionfx.mechanics.Game;
+import fr.umontpellier.iut.dominionfx.mechanics.Player;
+import fr.umontpellier.iut.dominionfx.mechanics.cards.Card;
+import fr.umontpellier.iut.dominionfx.mechanics.playerstate.StartTurnState;
+import fr.umontpellier.iut.dominionfx.mechanics.playerstate.TreasurePhase;
+import fr.umontpellier.iut.dominionfx.mechanics.playerstate.ongoingactions.durations.TidePoolsState;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SeveralPlayersTest extends BaseTestClass {
+
+    @Override
+    public void start(Stage stage) {
+        game = new Game(new String[]{"PlayerTest1", "PlayerTest2", "PlayerTest3", "PlayerTest4"},
+                        new String[]{"Bazaar", "Cutpurse"});
+        super.start(stage);
+    }
+
+    @Test
+    public void allPlayersProcessed() {
+        Player currentPlayer = game.currentPlayer();
+        addToSecondPlayersHand("Cutpurse");
+        clickOnSkip();
+        clickOnSkip();
+        clickOnSkip();
+        clickOnCardInHand("Cutpurse");
+        assertEquals(4, currentPlayer.getGame().getPlayers().get(0).getHand().size());
+        assertEquals(4, currentPlayer.getGame().getPlayers().get(1).getHand().size());
+        assertEquals(4, currentPlayer.getGame().getPlayers().get(2).getHand().size());
+//        pause(2);
+    }
+}
